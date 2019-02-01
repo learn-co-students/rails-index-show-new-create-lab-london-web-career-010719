8bc1e3ed94053a9a1b33e14a917108380085f09a
Chris Camacho <cacamacho91@gmail.com>
2019-02-01 12:03:16 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.destroy_all
+
+coups = [
+  {coupon_code: "8732985797" , store: "Debenhams"},
+  {coupon_code: "1234234123" , store: "H&M"},
+  {coupon_code: "482ty10594" , store: "H&M"},
+  {coupon_code: "23hfdn2345" , store: "Debenhams"},
+  {coupon_code: "12387hsf38" , store: "Apple"}
+]
+
+Coupon.create(coups)
--- spec/features/coupon_spec.rb
+++ spec/features/coupon_spec.rb
@@ -28,7 +28,7 @@ describe 'form page' do
     fill_in 'coupon[coupon_code]', with: "YAYFREE"
     fill_in 'coupon[store]', with: "Hobby Lobby"
 
-    click_on "Submit Coupon"
+    click_on "Create Coupon"
 
     expect(page).to have_content("YAYFREE")
   end
@@ -39,7 +39,7 @@ describe 'form page' do
     fill_in 'coupon[coupon_code]', with: "FREEITEM"
     fill_in 'coupon[store]', with: "Quip"
 
-    click_on "Submit Coupon"
+    click_on "Create Coupon"
 
     expect(Coupon.last.store).to eq("Quip")
   end
@@ -70,6 +70,6 @@ describe 'linking from the index page to the show page' do
   it 'index page links to coupon page' do
     linked_coupon = Coupon.create(coupon_code: "FREESTUFF", store: "Chipotle")
     visit coupons_path
-    expect(page).to have_link(linked_coupon.coupon_code, href: coupon_path(linked_coupon))
+    expect(page).to have_link(linked_coupon.to_s, href: coupon_path(linked_coupon))
   end
 end
